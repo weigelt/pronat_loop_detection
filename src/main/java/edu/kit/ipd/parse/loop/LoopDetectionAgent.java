17daf62850cdae8f33736597fee2f67d0ab70e78
Sebastian Weigelt <weigelt@kit.edu>
2018-03-02 16:15:21 +0100

    basic agent definition

---
+++ src/main/java/edu/kit/ipd/parse/loop/LoopDetectionAgent.java
@@ -0,0 +1,22 @@
+package edu.kit.ipd.parse.loop;
+
+import org.kohsuke.MetaInfServices;
+
+import edu.kit.ipd.parse.luna.agent.AbstractAgent;
+
+@MetaInfServices(AbstractAgent.class)
+public class LoopDetectionAgent extends AbstractAgent {
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void exec() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
