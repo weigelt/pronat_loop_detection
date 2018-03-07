f9135bce262ac6b92d0b07e6a24b1f5094a8272d
Sebastian Weigelt <weigelt@kit.edu>
2018-03-07 17:55:58 +0100

    ending while loop gramar filter#

---
+++ src/main/java/edu/kit/ipd/parse/loop/filter/EndingWhileLoopGrammarFilter.java
@@ -0,0 +1,25 @@
+package edu.kit.ipd.parse.loop.filter;
+
+import edu.kit.ipd.parse.loop.data.Keyphrase;
+import edu.kit.ipd.parse.loop.data.Loop;
+import edu.kit.ipd.parse.luna.data.MissingDataException;
+import edu.kit.ipd.parse.luna.graph.INode;
+
+public class EndingWhileLoopGrammarFilter extends AbstractWhileLoopGrammarFilter {
+
+	public EndingWhileLoopGrammarFilter() {
+		super();
+	}
+
+	@Override
+	protected Loop constructLoop(Keyphrase keyphrase) throws MissingDataException {
+		INode start = keyphrase.getAttachedNodes().get(0);
+		INode leftAction = GrammarFilter.findActionNodes(start, true);
+		if (leftAction == null) {
+			return null;
+		} else {
+			return ldne.extract(keyphrase, leftAction, true);
+		}
+	}
+
+}
