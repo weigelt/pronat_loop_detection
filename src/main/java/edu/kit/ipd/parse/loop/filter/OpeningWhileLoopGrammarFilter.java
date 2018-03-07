e58ba54ee3a95d6e6c37c395ef7f2a2d35f45540
Sebastian Weigelt <weigelt@kit.edu>
2018-03-07 17:56:23 +0100

    opening while loop gramar filter

---
+++ src/main/java/edu/kit/ipd/parse/loop/filter/OpeningWhileLoopGrammarFilter.java
@@ -0,0 +1,25 @@
+package edu.kit.ipd.parse.loop.filter;
+
+import edu.kit.ipd.parse.loop.data.Keyphrase;
+import edu.kit.ipd.parse.loop.data.Loop;
+import edu.kit.ipd.parse.luna.data.MissingDataException;
+import edu.kit.ipd.parse.luna.graph.INode;
+
+public class OpeningWhileLoopGrammarFilter extends AbstractWhileLoopGrammarFilter {
+
+	public OpeningWhileLoopGrammarFilter() {
+		super();
+	}
+
+	@Override
+	protected Loop constructLoop(Keyphrase keyphrase) throws MissingDataException {
+		INode start = keyphrase.getConditionNodes().get(keyphrase.getConditionNodes().size() - 1);
+		INode rightAction = GrammarFilter.findActionNodes(start, false);
+		if (rightAction == null) {
+			return null;
+		} else {
+			return ldne.extract(keyphrase, rightAction, false);
+		}
+	}
+
+}
