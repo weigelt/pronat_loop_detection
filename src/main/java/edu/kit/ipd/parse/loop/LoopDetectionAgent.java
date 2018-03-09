3ec07c10c843f700f8e4639728efad7ddae4a60f
Tobias Hey <hey@kit.edu>
2018-03-09 16:13:09 +0100

    write to graph wip

--- src/main/java/edu/kit/ipd/parse/loop/LoopDetectionAgent.java
+++ src/main/java/edu/kit/ipd/parse/loop/LoopDetectionAgent.java
@@ -5,13 +5,16 @@ import java.util.List;
 import org.kohsuke.MetaInfServices;
 
 import edu.kit.ipd.parse.loop.data.Keyphrase;
+import edu.kit.ipd.parse.loop.data.KeyphraseType;
 import edu.kit.ipd.parse.loop.data.Loop;
 import edu.kit.ipd.parse.loop.data.Utterance;
 import edu.kit.ipd.parse.loop.filter.GrammarFilter;
 import edu.kit.ipd.parse.loop.filter.KeyphraseFilter;
 import edu.kit.ipd.parse.luna.agent.AbstractAgent;
 import edu.kit.ipd.parse.luna.data.MissingDataException;
+import edu.kit.ipd.parse.luna.graph.IArc;
 import edu.kit.ipd.parse.luna.graph.IArcType;
+import edu.kit.ipd.parse.luna.graph.INode;
 import edu.kit.ipd.parse.luna.graph.INodeType;
 import edu.kit.ipd.parse.luna.graph.ParseGraph;
 
@@ -25,6 +28,14 @@ public class LoopDetectionAgent extends AbstractAgent {
 	private static final String ARC_TYPE_DEPENDENT_ACTION = "dependentLoopAction";
 	private static final String ARC_TYPE_CONDITION = "loopCondition";
 	private static final String NODE_TYPE_LOOP = "loop";
+
+	private static final String ATTRIBUTE_NAME_CONDITION = "condition";
+	private static final String ATTRIBUTE_NAME_DEPENDENT_PHRASES = "dependentPhrases";
+	private static final String ATTRIBUTE_NAME_KEYPHRASE_TYPE = "keyphraseType";
+	private static final String ATTRIBUTE_NAME_KEYPHRASE = "keyphrase";
+	private static final String ATTRIBUTE_NAME_POSITION = "position";
+	private static final String ATTRIBUTE_NAME_TYPE = "type";
+	private static final String ATTRIBUTE_NAME_VERFIED_BY_DA = "verfiedByDA";
 
 	private IArcType keyPhraseArcType;
 	private IArcType dependentActionArcType;
@@ -68,7 +79,104 @@ public class LoopDetectionAgent extends AbstractAgent {
 	}
 
 	private void writeToGraph(List<Loop> loops) {
+		INodeType tokenType = graph.getNodeType(NODE_TYPE_TOKEN);
+		for (Loop loop : loops) {
+			Keyphrase currKPtoWrite = loop.getKeyphrase();
+			INode currLoopNode = null;
+			for (int i = 0; i < currKPtoWrite.getAttachedNodes().size(); i++) {
+				INode currKPNode = currKPtoWrite.getAttachedNodes().get(i);
+				if (i == 0) {
+					if (!currKPNode.getIncomingArcsOfType(keyPhraseArcType).isEmpty()
+							&& currKPNode.getIncomingArcsOfType(keyPhraseArcType).get(0).getSourceNode().getType().equals(loopNodeType)) {
+						// we already have a loop node and this is also the source
+						// TODO: what if the source node is not the loop node but a token node instead?
+						currLoopNode = currKPNode.getIncomingArcsOfType(keyPhraseArcType).get(0).getSourceNode();
+					} else {
+						// we don't have a loop node. Thus, we create one!
+						currLoopNode = graph.createNode(loopNodeType);
+						currLoopNode.setAttributeValue(ATTRIBUTE_NAME_TYPE, loop.getType().name());
+						currLoopNode.setAttributeValue(ATTRIBUTE_NAME_KEYPHRASE, currKPtoWrite.getKeyphraseAsString());
+						String keyphraseType = convertTypeToString(currKPtoWrite);
+						currLoopNode.setAttributeValue(ATTRIBUTE_NAME_TYPE, keyphraseType);
+						currLoopNode.setAttributeValue(ATTRIBUTE_NAME_DEPENDENT_PHRASES, loop.getDependentPhrasesAsString());
+						currLoopNode.setAttributeValue(ATTRIBUTE_NAME_CONDITION, loop.getConditionAsString());
+						IArc newArc = graph.createArc(currLoopNode, currKPNode, keyPhraseArcType);
+						// create according arc
+						newArc.setAttributeValue(ATTRIBUTE_NAME_VERFIED_BY_DA, false);
+						newArc.setAttributeValue(ATTRIBUTE_NAME_TYPE, keyphraseType);
+						// create dep action links
+						for (int j = 0; j < loop.getDependentActions().size(); j++) {
+							IArc currDepArc = graph.createArc(currLoopNode, loop.getDependentActions().get(j), dependentActionArcType);
+							currDepArc.setAttributeValue(ATTRIBUTE_NAME_VERFIED_BY_DA, false);
+							currDepArc.setAttributeValue(ATTRIBUTE_NAME_POSITION, j);
+						}
+						//TODO: ADD CONDTION
+					}
+				} else {
+					// i>0
+					if (!currKPNode.getIncomingArcsOfType(keyPhraseArcType).isEmpty()) {
+						// we already have keyphrase arc
+						if (currKPNode.getIncomingArcsOfType(keyPhraseArcType).get(0).getSourceNode().getType().equals(loopNodeType)) {
+							// but it points to a node o.0
+							if ((boolean) currKPNode.getIncomingArcsOfType(keyPhraseArcType).get(0)
+									.getAttributeValue(ATTRIBUTE_NAME_VERFIED_BY_DA)) {
+								//TODO: might crash iff "verfiedByDA" is unset
+								//but it has been verfied by the da
+								cleanUp(currLoopNode);
+								// set the new curr con act node
+								currLoopNode = currKPNode.getIncomingArcsOfType(keyPhraseArcType).get(0).getSourceNode();
+								// we have to clean up the previously build con node
+								//TODO: what if null?
+							}
+							// we have to clean up!
+							cleanUp(currKPNode.getIncomingArcsOfType(keyPhraseArcType).get(0).getSourceNode());
 
+						} else if (currKPNode.getIncomingArcsOfType(keyPhraseArcType).get(0).getSourceNode().getType().equals(tokenType)) {
+							//and it points to a token node...
+							if (!currKPNode.getIncomingArcsOfType(keyPhraseArcType).get(0).getSourceNode()
+									.equals(currKPtoWrite.getAttachedNodes().get(i - 1))) {
+								// but it's not the right one
+								// TODO: what now? Throw an exception?
+							}
+							// it's the right node... everything's fine!
+						}
+					} else {
+						// that's the good case. We simply add a new arc from the first to the next node of the keyphrase
+						// create intermediate arc
+						createKeyPhraseArc(currKPtoWrite.getAttachedNodes().get(i - 1), currKPNode, convertTypeToString(currKPtoWrite));
+					}
+				}
+			}
+		}
+	}
+
+	private void cleanUp(INode sourceNode) {
+		if (sourceNode == null) {
+			return;
+		}
+		for (IArc depAction : sourceNode.getOutgoingArcsOfType(dependentActionArcType)) {
+			graph.deleteArc(depAction);
+		}
+		for (IArc keyPhrase : sourceNode.getOutgoingArcsOfType(keyPhraseArcType)) {
+			INode nextNode = keyPhrase.getTargetNode();
+			graph.deleteArc(keyPhrase);
+
+			while (nextNode.getIncomingArcsOfType(keyPhraseArcType).size() < 1) {
+				// we stop when the node has more than one incomming keyphrase arcs,
+				// i.e. it has one that comes from a conc Action node and one that comes from a token
+				// (this one was deleted in the step before)
+				IArc nextArc = nextNode.getOutgoingArcsOfType(keyPhraseArcType).get(0);
+				nextNode = nextArc.getTargetNode();
+				graph.deleteArc(nextArc);
+			}
+		}
+	}
+
+	private void createKeyPhraseArc(INode from, INode to, String type) {
+		IArc newArc = graph.createArc(from, to, keyPhraseArcType);
+		// create according arc
+		newArc.setAttributeValue("verfiedByDA", false);
+		newArc.setAttributeValue("type", type);
 	}
 
 	/**
@@ -90,8 +198,8 @@ public class LoopDetectionAgent extends AbstractAgent {
 	private IArcType createKeyphraseArcType() {
 		if (!graph.hasArcType(ARC_TYPE_KEY_PHRASE)) {
 			IArcType kpat = graph.createArcType(ARC_TYPE_KEY_PHRASE);
-			kpat.addAttributeToType("String", "verfiedByDA");
-			kpat.addAttributeToType("String", "type");
+			kpat.addAttributeToType("String", ATTRIBUTE_NAME_VERFIED_BY_DA);
+			kpat.addAttributeToType("String", ATTRIBUTE_NAME_TYPE);
 			return kpat;
 		} else {
 			return graph.getArcType(ARC_TYPE_KEY_PHRASE);
@@ -101,8 +209,8 @@ public class LoopDetectionAgent extends AbstractAgent {
 	private IArcType createDependentActionArcType() {
 		if (!graph.hasArcType(ARC_TYPE_DEPENDENT_ACTION)) {
 			IArcType daat = graph.createArcType(ARC_TYPE_DEPENDENT_ACTION);
-			daat.addAttributeToType("int", "position");
-			daat.addAttributeToType("String", "verfiedByDA");
+			daat.addAttributeToType("int", ATTRIBUTE_NAME_POSITION);
+			daat.addAttributeToType("String", ATTRIBUTE_NAME_VERFIED_BY_DA);
 			return daat;
 		} else {
 			return graph.getArcType(ARC_TYPE_DEPENDENT_ACTION);
@@ -112,7 +220,7 @@ public class LoopDetectionAgent extends AbstractAgent {
 	private IArcType createConditionArcType() {
 		if (!graph.hasArcType(ARC_TYPE_CONDITION)) {
 			IArcType cat = graph.createArcType(ARC_TYPE_CONDITION);
-			cat.addAttributeToType("String", "verfiedByDA");
+			cat.addAttributeToType("String", ATTRIBUTE_NAME_VERFIED_BY_DA);
 			return cat;
 		} else {
 			return graph.getArcType(ARC_TYPE_CONDITION);
@@ -122,14 +230,20 @@ public class LoopDetectionAgent extends AbstractAgent {
 	private INodeType createLoopNodeType() {
 		if (!graph.hasNodeType(NODE_TYPE_LOOP)) {
 			INodeType lont = graph.createNodeType(NODE_TYPE_LOOP);
-			lont.addAttributeToType("String", "type");
-			lont.addAttributeToType("String", "keyphrase");
-			lont.addAttributeToType("String", "keyphraseType");
-			lont.addAttributeToType("String", "dependentPhrases");
+			lont.addAttributeToType("String", ATTRIBUTE_NAME_TYPE);
+			lont.addAttributeToType("String", ATTRIBUTE_NAME_KEYPHRASE);
+			lont.addAttributeToType("String", ATTRIBUTE_NAME_KEYPHRASE_TYPE);
+			lont.addAttributeToType("String", ATTRIBUTE_NAME_DEPENDENT_PHRASES);
+			lont.addAttributeToType("String", ATTRIBUTE_NAME_CONDITION);
 			return lont;
 		} else {
 			return graph.getNodeType(NODE_TYPE_LOOP);
 		}
+	}
+
+	private String convertTypeToString(Keyphrase currKPtoWrite) {
+		return currKPtoWrite.getSecondaryType().equals(KeyphraseType.UNSET) ? currKPtoWrite.getPrimaryType().name()
+				: currKPtoWrite.getPrimaryType() + "/" + currKPtoWrite.getSecondaryType().name();
 	}
 
 }
